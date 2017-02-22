d46e9b8521ed1460f201b77cb4a4860bcdb67711
Daniel Parker <dparker.tech@gmail.com>
2017-02-22 14:05:36 +1100

    Read all messages not just some

--- slackcloud.py
+++ slackcloud.py
@@ -27,12 +27,51 @@ slack_cmd1_name = os.environ.get('SLACK_CMD1_NAME')
 slack_cmd1_token = os.environ.get('SLACK_CMD1_TOKEN')
 port = int(os.environ.get("PORT", 8081))
 
+def extract_messages(messages):
+    msgs = []
+    latestts = None
+    oldestts = None
+
+    for i in range(len(messages)):
+        if messages[i]['type'] == 'message':
+            try:
+                subtype = messages[i]['subtype']
+            except:
+                msgs.append(messages[i]['text'])
+
+        if i == 0:
+            latestts = messages[i]['ts']
+
+        if i == len(messages) - 1:
+            oldestts = messages[i]['ts']
+
+    return msgs, latestts, oldestts
 
 def upload_wordcloud(channel_id, channel_name, **kwargs):
     slack = Slacker(slack_auth)
 
-    # Grab last 100 messages in channel_id
-    msgs = [i['text'] for i in slack.channels.history(channel_id).body['messages']]
+    msgs = []
+    latestts = None
+    oldestts = None
+    hasMore = True
+
+    while hasMore:
+        if latestts is None:
+            result = slack.channels.history(channel_id, None, None, 1000)
+        else:
+            result = slack.channels.history(channel_id, latestts, oldestts, 1000)
+
+        if result.body['has_more'] != "true":
+            hasMore = False
+
+        messages, latestts, oldestts = extract_messages(result.body['messages'])
+        msgs.extend(messages)
+
+        if not hasMore:
+            break
+        
+        print 'has more'
+
     logging.debug(msgs)
     try:
         # Concat all the words
@@ -47,7 +86,6 @@ def upload_wordcloud(channel_id, channel_name, **kwargs):
                            initial_comment=initial_comment)
     except TypeError as e:
         logging.error(e)
-
 
 @slack.command(command=slack_cmd1_name, token=slack_cmd1_token,
                team_id=slack_teamid, methods=['POST'])
